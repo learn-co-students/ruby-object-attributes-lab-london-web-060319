a5582392de85ccf65ffce6599b45b1b8f80ffd6b
Federica Macchi <federica.macchi3@gmail.com>
2019-06-05 09:37:18 +0100

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+    def name
+        @name
+    end
+
+    def name=(person_name)
+        @name = person_name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(persons_job)
+        @job = persons_job
+    end
+
+end
